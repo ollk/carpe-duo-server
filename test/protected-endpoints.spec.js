8c6e800ff37109074654468ca530727895e6d37a
Scott Williams <scottcolemanwilliams@gmail.com>
2019-05-19 11:40:21 -0500

    added protected endpoints testing

---
+++ test/protected-endpoints.spec.js
@@ -0,0 +1,104 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const knex = require('knex');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+describe('Protected endpoints', function() {
+  let db;
+
+  const {
+    testUsers,
+    testTasks
+  } = helpers.makeFixtures();
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('cleanup', () => helpers.cleanTables(db));
+
+  afterEach('cleanup', () => helpers.cleanTables(db));
+
+  beforeEach('insert users/tasks', () => 
+    helpers.seedTables(
+      db,
+      testUsers,
+      testTasks
+    )
+  );
+
+  const protectedEndpoints = [
+    {
+      name: 'GET /api/sleep/:user_id',
+      path: '/api/sleep/1',
+      method: supertest(app).get
+    },
+    {
+      name: 'POST /api/sleep',
+      path: '/api/sleep',
+      method: supertest(app).post
+    },
+    {
+      name: 'GET /api/tasks/:user_id',
+      path: '/api/tasks/1',
+      method: supertest(app).get
+    },
+    {
+      name: 'POST /api/tasks',
+      path: '/api/tasks',
+      method: supertest(app).post
+    },
+    {
+      name: 'POST /api/tasks/:id',
+      path: '/api/tasks/1',
+      method: supertest(app).post
+    },
+    {
+      name: 'POST /api/tasks/reset/:userId',
+      path: '/api/tasks/reset/1',
+      method: supertest(app).post
+    },
+    {
+      name: 'DELETE /api/tasks/:id',
+      path: '/api/tasks/1',
+      method: supertest(app).delete
+    },
+    {
+      name: 'POST /api/auth/refresh',
+      path: '/api/auth/refresh',
+      method: supertest(app).post
+    }
+  ];
+
+  protectedEndpoints.forEach(endpoint => {
+    describe(endpoint.name, () => {
+      it('responds 401 \'Missing bearer token\' when no bearer token', () => {
+        return endpoint.method(endpoint.path)
+          .expect(401, {error: 'Missing bearer token'});
+      });
+
+      it('responds 401 \'Unauthorized request\' when invalid JWT secret', () => {
+        const validUser = testUsers[0];
+        const invalidSecret = 'bad-secret';
+        return endpoint.method(endpoint.path)
+          .set('Authorization', helpers.makeAuthHeader(validUser, invalidSecret))
+          .expect(401, {error: 'Unauthorized request'});
+      });
+
+      it('responds 401 \'Unauthorized request\' when invalid sub in payload', () => {
+        const invalidUser = {user_name: 'user-not-existy', id: 1};
+        return endpoint.method(endpoint.path)
+          .set('Authorization', helpers.makeAuthHeader(invalidUser))
+          .expect(401, {error: 'Unauthorized request'});
+      });
+    });
+  });
+});
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -44,14 +44,56 @@ function makeUsersArray() {
       sat_bed: 44,
       sun_wake: 62,
       sun_bed: 92
+    }
+  ];
+}
+
+function makeTasksArray(users) {
+  return [
+    {
+      id: 1,
+      task_name: 'test task 1',
+      duration: 30,
+      scheduled: true,
+      position: 60,
+      priority: 'low',
+      user_id: users[0].id
     },
+    {
+      id: 2,
+      task_name: 'test task 2',
+      duration: 60,
+      scheduled: false,
+      position: 0,
+      priority: 'medium',
+      user_id: users[1].id
+    },
+    {
+      id: 3,
+      task_name: 'test task 3',
+      duration: 90,
+      scheduled: true,
+      position: 120,
+      priority: 'high',
+      user_id: users[2].id
+    },
+    {
+      id: 4,
+      task_name: 'test task 4',
+      duration: 120,
+      scheduled: false,
+      position: 0,
+      priority: 'low',
+      user_id: users[3].id
+    }
   ];
 }
 
 function makeFixtures() {
   const testUsers = makeUsersArray();
+  const testTasks = makeTasksArray(testUsers)
 
-  return { testUsers };
+  return { testUsers, testTasks };
 }
 
 //TODO: may need to fix this
@@ -78,6 +120,15 @@ function seedUsers(db, users) {
     )
 }
 
+function seedTables(db, users, tasks) {
+  return seedUsers(db, users)
+    .then(() => 
+      db 
+        .into('carpeduo_tasks')
+        .insert(tasks)
+    )
+}
+
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
   const token = jwt.sign({user_id: user.id}, secret, {
     subject: user.user_name,
@@ -92,5 +143,6 @@ module.exports = {
   makeFixtures,
   cleanTables,
   seedUsers,
-  makeAuthHeader
+  makeAuthHeader,
+  seedTables
 };
